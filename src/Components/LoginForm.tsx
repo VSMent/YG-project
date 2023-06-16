59f22d6cbb520c2876cc18b08bf6b641396588d2
VSMent <vsmentvs@gmail.com>
2023-06-16 23:55:10 +0300

    some kind of login logic

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,47 +1,9 @@
 import styles from '../styles/Login.module.sass'
-import { useRef, useState } from 'react'
-import { loadFromLS, saveToLS } from '../Utils/LocalStorage'
-import hashPassword from '../Utils/HashPassword'
-import { LS_ITEM_USERS } from '../data/constants'
-import User from '../data/User'
+import { useRef } from 'react'
 
-const correctPass = (user: User, pass: string) => user.pass === pass
-
-const LoginForm = () => {
-  const [users, setUsers] = useState(loadFromLS(LS_ITEM_USERS) ?? [])
-
+const LoginForm = ({ loginHandler, registerHandler }: LoginFormProps) => {
   const loginInput = useRef<HTMLInputElement>(null)
   const passInput = useRef<HTMLInputElement>(null)
-
-  const findUser = (login: string) => {
-    const foundUser = users.filter((u: User) => u.login === login)
-    return foundUser.length === 1 ? foundUser[0] : null
-  }
-
-  const handleLogin = async (login: string, pass: string) => {
-    console.log(login, pass)
-    const hashedPass = await hashPassword(pass)
-    const user = findUser(login)
-    if (user && correctPass(user, hashedPass)) {
-      console.log(`logged in\nhello${login}`)
-    } else {
-      console.error('wrong pass')
-    }
-  }
-  const handleRegister = async (login: string, pass: string) => {
-    const user = findUser(login)
-    if (!user) {
-      const hashedPass = await hashPassword(pass)
-      const newUser = new User(login, hashedPass)
-      const newUsers = [...users, newUser]
-      console.log(users, newUsers)
-      setUsers([...newUsers])
-      saveToLS(LS_ITEM_USERS, newUsers)
-      console.log(`registered)`)
-    } else {
-      console.log(`user exists\n login please`)
-    }
-  }
 
   return <>
     <div className={styles.Login}>
@@ -52,16 +14,20 @@ const LoginForm = () => {
       <input id="pass" type="password" name="pass" ref={passInput}/>
       <button type="button"
               onClick={() => {
-                handleLogin(loginInput.current?.value ?? '', passInput.current?.value ?? '').catch(console.error)
+                loginHandler(loginInput.current?.value ?? '', passInput.current?.value ?? '').catch(console.error)
               }}>Login
       </button>
       <button type="button"
               onClick={() => {
-                handleRegister(loginInput.current?.value ?? '', passInput.current?.value ?? '').catch(console.error)
+                registerHandler(loginInput.current?.value ?? '', passInput.current?.value ?? '').catch(console.error)
               }}>Register
       </button>
     </div>
   </>
 }
 
+type  LoginFormProps = {
+  loginHandler: (login: string, pass: string) => Promise<void>
+  registerHandler: (login: string, pass: string) => Promise<void>
+}
 export default LoginForm
--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,12 +1,49 @@
 import LoginForm from '../Components/LoginForm'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
+import hashPassword from '../Utils/HashPassword'
+import User from '../data/User'
+import { loadFromLS, saveToLS } from '../Utils/LocalStorage'
+import { LS_ITEM_USERS } from '../data/constants'
 
+const correctPass = (user: User, pass: string) => user.pass === pass
 const LoginPage = () => {
   const navigate = useNavigate()
+
+  const [users, setUsers] = useState(loadFromLS(LS_ITEM_USERS) ?? [])
+  const findUser = (login: string) => {
+    const foundUser = users.filter((u: User) => u.login === login)
+    return foundUser.length === 1 ? foundUser[0] : null
+  }
+  const loginHandler = async (login: string, pass: string) => {
+    console.log(login, pass)
+    const hashedPass = await hashPassword(pass)
+    const user = findUser(login)
+    if (user && correctPass(user, hashedPass)) {
+      console.log(`logged in\nhello${login}`)
+      navigate('/')
+    } else {
+      console.error('wrong pass')
+    }
+  }
+  const registerHandler = async (login: string, pass: string) => {
+    const user = findUser(login)
+    if (!user) {
+      const hashedPass = await hashPassword(pass)
+      const newUser = new User(login, hashedPass)
+      const newUsers = [...users, newUser]
+      console.log(users, newUsers)
+      setUsers([...newUsers])
+      saveToLS(LS_ITEM_USERS, newUsers)
+      console.log(`registered)`)
+    } else {
+      console.log(`user exists\n login please`)
+    }
+  }
+
   return <>
-    <LoginForm/>
-    <button type='button' onClick={()=>navigate('/')}>Auth</button>
+    <LoginForm loginHandler={loginHandler}
+               registerHandler={registerHandler}/>
   </>
 }
 
