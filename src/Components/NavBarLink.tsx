b7eca1181219a4ffd29eaabc4e5d2c2a1d586c0b
VSMent <vsmentvs@gmail.com>
2023-06-18 00:07:26 +0300

    nav bar styles 1.0

--- src/Components/NavBarLink.tsx
+++ src/Components/NavBarLink.tsx
@@ -5,16 +5,26 @@ const NavBarLink = ({ to, icon, name }: NavBarLinkProps) => {
   const Icon = cloneElement(icon, { className: 'h-full w-full' })
 
   return (
-    <>
+    <div className="group relative transition-all ease-in">
       <NavLink
         to={to}
-        className="mb-1 flex aspect-square w-full items-center justify-center rounded-xl bg-red-400 text-center"
+        className={
+          'mb-1 flex aspect-square w-full items-center justify-center rounded-xl text-center text-gray-400 ' +
+          'duration-200 last:mb-0 group-hover:text-black group-hover:shadow-md'
+        }
       >
-        {/*<div className="">{(icon.props = { color: 'pink' })}</div>*/}
         <div className="h-1/2">{Icon}</div>
-        {/*{name}*/}
       </NavLink>
-    </>
+      <div
+        className={
+          'invisible absolute bottom-4 left-20 z-[1] inline-block w-40 rounded-xl bg-white p-2 text-center ' +
+          'text-black opacity-0 duration-200 delay-0 ' +
+          'group-hover:visible group-hover:opacity-100 group-hover:duration-1000 group-hover:delay-1000'
+        }
+      >
+        {name}
+      </div>
+    </div>
   )
 }
 
