2872a069d1145b92579ae2309a2065327e9cd5f3
VSMent <vsmentvs@gmail.com>
2023-06-17 18:26:47 +0300

    tailwind watch script prettier with tailwind plugin

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+  globals: {},
+  rules: {
+    'prettier/prettier': ['error', {
+      'endOfLine': 'auto'
+    }]
+  }
+}
