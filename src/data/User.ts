39710faedebe80a1e9a52740795b8d0269a13c34
VSMent <vsmentvs@gmail.com>
2023-06-25 01:42:18 +0300

    add role to User class

--- src/data/User.ts
+++ src/data/User.ts
@@ -1,10 +1,14 @@
+type Role = 'admin' | 'user'
+
 class User {
   login: string
   pass: string
+  role: Role
 
-  constructor(_login: string, _pass: string) {
+  constructor(_login: string, _pass: string, _role: Role = 'user') {
     this.login = _login
     this.pass = _pass
+    this.role = _role
   }
 }
 
