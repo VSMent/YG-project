51d8d92bd8a594022ae8fb80d4ef3c0aaadc435e
VSMent <vsmentvs@gmail.com>
2023-06-25 20:11:44 +0300

    add gh-pages module and scripts add env file to properly configure build for gh-pages add SPA for GHP hack to work with browser router in gh-pages

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -3,51 +3,57 @@ import React from 'react'
 import { NAV_LINKS } from '../data/constants'
 import MainPage from '../Pages/MainPage'
 import LoginPage from '../Pages/LoginPage'
+import ErrorPage from '../Pages/ErrorPage'
 import Layout from './Layout'
 
-const Router = createBrowserRouter([
-  {
-    path: NAV_LINKS.LOGIN,
-    element: <LoginPage />,
-  },
-  {
-    path: NAV_LINKS.MAIN,
-    element: <Layout />,
-    children: [
-      {
-        element: <MainPage />,
-        index: true,
-      },
-      {
-        path: NAV_LINKS.EMPLOYEES,
-        element: <>{NAV_LINKS.EMPLOYEES}</>,
-      },
-      {
-        path: NAV_LINKS.CLIENTS,
-        element: <>{NAV_LINKS.CLIENTS}</>,
-      },
-      {
-        path: NAV_LINKS.PERSONNEL,
-        element: <>{NAV_LINKS.PERSONNEL}</>,
-      },
-      {
-        path: NAV_LINKS.FINANCES,
-        element: <>{NAV_LINKS.FINANCES}</>,
-      },
-      {
-        path: NAV_LINKS.PRODUCTION,
-        element: <>{NAV_LINKS.PRODUCTION}</>,
-      },
-      {
-        path: NAV_LINKS.COMMUNICATION,
-        element: <>{NAV_LINKS.COMMUNICATION}</>,
-      },
-      {
-        path: NAV_LINKS.SELF_MANAGEMENT,
-        element: <>{NAV_LINKS.SELF_MANAGEMENT}</>,
-      },
-    ],
-  },
-])
+const Router = createBrowserRouter(
+  [
+    {
+      path: NAV_LINKS.LOGIN,
+      element: <LoginPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: NAV_LINKS.MAIN,
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          element: <MainPage />,
+          index: true,
+        },
+        {
+          path: NAV_LINKS.EMPLOYEES,
+          element: <>{NAV_LINKS.EMPLOYEES}</>,
+        },
+        {
+          path: NAV_LINKS.CLIENTS,
+          element: <>{NAV_LINKS.CLIENTS}</>,
+        },
+        {
+          path: NAV_LINKS.PERSONNEL,
+          element: <>{NAV_LINKS.PERSONNEL}</>,
+        },
+        {
+          path: NAV_LINKS.FINANCES,
+          element: <>{NAV_LINKS.FINANCES}</>,
+        },
+        {
+          path: NAV_LINKS.PRODUCTION,
+          element: <>{NAV_LINKS.PRODUCTION}</>,
+        },
+        {
+          path: NAV_LINKS.COMMUNICATION,
+          element: <>{NAV_LINKS.COMMUNICATION}</>,
+        },
+        {
+          path: NAV_LINKS.SELF_MANAGEMENT,
+          element: <>{NAV_LINKS.SELF_MANAGEMENT}</>,
+        },
+      ],
+    },
+  ],
+  { basename: `${process.env.REACT_APP_ROUTER_BASE_NAME}` }
+)
 
 export default Router
