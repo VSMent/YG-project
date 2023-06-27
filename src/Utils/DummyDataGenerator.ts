d3c826d15e989f1ff13ebd202f64bf884c9139be
VSMent <vsmentvs@gmail.com>
2023-06-27 14:21:39 +0300

    add more fields to User class, update dummy generator

--- src/Utils/DummyDataGenerator.ts
+++ src/Utils/DummyDataGenerator.ts
@@ -3,7 +3,87 @@ import User from '../data/User'
 import hashPassword from './HashPassword'
 import { useUserStore } from './Stores'
 
+const peopleData = {
+  firstnames: [
+    'Віктор',
+    'Борис',
+    'Єлизавета',
+    'Федір',
+    'Ярослав',
+    'Тамара',
+    'Андрій',
+    'Тарас',
+    'Віталій',
+    'Володимир',
+    'Олена',
+    'Ірина',
+    'Оксана',
+    'Ілля',
+    'Лілія',
+    'Максим',
+  ],
+  lastnames: [
+    'Ткачук',
+    'Шевчук',
+    'Кравченко',
+    'Василенко',
+    'Когут',
+    'Стасюк',
+    'Василишин',
+    'Павленко',
+    'Іванов',
+    'Олійник',
+    'Ковальчук',
+    'Лисенко',
+    'Баран',
+  ],
+  usernames: ['intega', 'parin', 'ystn', 'vtli', 'omian'],
+}
 const useDummyUserData = () => {
+  const { users, addUser } = useUserStore()
+
+  useEffect(() => {
+    const add = async () => {
+      if (users.length == 0) {
+        addUser(
+          new User(
+            'admin@gmail.com',
+            await hashPassword('admin'),
+            'Максим',
+            'Стасюк',
+            'admin'
+          )
+        )
+        addUser(
+          new User(
+            'user@gmail.com',
+            await hashPassword('user'),
+            'Олена',
+            'Павленко'
+          )
+        )
+        for (const username of peopleData.usernames) {
+          addUser(
+            new User(
+              `${username}${
+                Math.random() > 0.5 ? '@gmail.com' : '@outlook.com'
+              }`,
+              await hashPassword(username),
+              peopleData.firstnames[
+                Math.floor(Math.random() * peopleData.firstnames.length)
+              ],
+              peopleData.lastnames[
+                Math.floor(Math.random() * peopleData.lastnames.length)
+              ]
+            )
+          )
+        }
+      }
+    }
+    add().catch(console.log)
+  })
+}
+const useDummyChatData = () => {
   const { users, addUser } = useUserStore()
 
   useEffect(() => {
--- src/data/User.ts
+++ src/data/User.ts
@@ -1,13 +1,23 @@
 type Role = 'admin' | 'user'
 
 class User {
+  firstname: string
+  lastname: string
   login: string
   pass: string
   role: Role
 
-  constructor(_login: string, _pass: string, _role: Role = 'user') {
+  constructor(
+    _login: string,
+    _pass: string,
+    _firstname: string = '',
+    _lastname: string = '',
+    _role: Role = 'user'
+  ) {
     this.login = _login
     this.pass = _pass
+    this.firstname = _firstname
+    this.lastname = _lastname
     this.role = _role
   }
 }
