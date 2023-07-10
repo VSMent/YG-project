036feb212d1c355d410b2e115b6c2d5240ea5aea
VSMent <vsmentvs@gmail.com>
2023-07-10 20:39:04 +0300

    EmployeesPage with scrollable cards

--- src/Pages/EmployeesPage.tsx
+++ src/Pages/EmployeesPage.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from '@radix-ui/react-icons'
 import {
   Accordion,
   AccordionContent,
@@ -12,10 +13,19 @@ import {
   CardTitle,
 } from '../shadcn-ui/components/ui/card'
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from '../shadcn-ui/components/ui/avatar'
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '../shadcn-ui/components/ui/popover'
+import { Button } from '../shadcn-ui/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from '../shadcn-ui/components/ui/command'
+import { ScrollArea } from '../Components/ScrollArea'
 
 const EmployeesPage = () => {
   return (
@@ -30,84 +40,119 @@ const EmployeesPage = () => {
           <AccordionItem value="personnel-department">
             <AccordionTrigger>Відділ Персоналу</AccordionTrigger>
             <AccordionContent>
-              <div className={'flex flex-row gap-2 '}>
-                {[1, 2, 3, 4, 5].map((el) => (
-                  // <div className={'bg-yellow-400'} >
-                  //   <h3></h3>
-                  // </div>
-                  <Card key={el}>
-                    <CardHeader>
-                      <CardTitle>
-                        <div className="flex items-center justify-between space-x-4">
-                          <div className="flex items-center space-x-4">
-                            <div>
-                              <p className="text-sm font-medium leading-none">
-                                Name{el} Surname{el}
-                              </p>
+              <div
+                className={
+                  '' +
+                  // 'flex flex-row gap-2 ' +
+                  ''
+                }
+              >
+                <ScrollArea
+                  orientation="horizontal"
+                  // type={'always'}
+                  className={
+                    'flex flex-[1_0_1px] flex-row rounded-md border p-4 text-base ' +
+                    // // fix radix viewport display
+                    '[&>[data-radix-scroll-area-viewport]>div]:!flex ' +
+                    '[&>[data-radix-scroll-area-viewport]>div]:flex-row ' +
+                    '[&>[data-radix-scroll-area-viewport]>div]:gap-3 '
+                  }
+                >
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((el) => (
+                    <Card className={''} key={el}>
+                      <CardHeader>
+                        <CardTitle>
+                          <div className="flex items-center justify-between space-x-4">
+                            <div className="flex items-center space-x-4">
+                              <div>
+                                <p className="text-sm font-medium leading-none">
+                                  Name{el} Surname{el}
+                                </p>
+                              </div>
                             </div>
                           </div>
+                        </CardTitle>
+                        <CardDescription>name{el}@gmail.com</CardDescription>
+                      </CardHeader>
+                      <CardContent className="flex w-max flex-row gap-2">
+                        <div className="flex flex-col">
+                          <div className={'flex justify-between gap-2'}>
+                            <p>Запланованих завдань:</p> <p>{el * 10}</p>
+                          </div>
+                          <div className={'flex justify-between gap-2'}>
+                            <p>Виконаних завдань:</p> <p>{el * 30}</p>
+                          </div>
+                          <div className={'flex justify-between gap-2'}>
+                            <p>Завдань у прогресі:</p> <p>{el * 50}</p>
+                          </div>
+                          <div className={'flex justify-between gap-2'}>
+                            <p>Виконаних завдань:</p> <p>{el * 70}</p>
+                          </div>
                         </div>
-                      </CardTitle>
-                      <CardDescription>name{el}@gmail.com</CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex flex-col">
-                      <div className={'flex justify-between gap-2'}>
-                        <p>Запланованих завдань:</p> <p>{el * 10}</p>
-                      </div>
-                      <div className={'flex justify-between gap-2'}>
-                        <p>Виконаних завдань:</p> <p>{el * 30}</p>
-                      </div>
-                      <div className={'flex justify-between gap-2'}>
-                        <p>Завдань у прогресі:</p> <p>{el * 50}</p>
-                      </div>
-                      <div className={'flex justify-between gap-2'}>
-                        <p>Виконаних завдань:</p> <p>{el * 70}</p>
-                      </div>
-                      {/*<Popover>*/}
-                      {/*  <PopoverTrigger asChild>*/}
-                      {/*    <Button variant="outline" className="ml-auto">*/}
-                      {/*      Owner{" "}*/}
-                      {/*      <ChevronDownIcon className="ml-2 h-4 w-4 text-muted-foreground" />*/}
-                      {/*    </Button>*/}
-                      {/*  </PopoverTrigger>*/}
-                      {/*  <PopoverContent className="p-0" align="end">*/}
-                      {/*    <Command>*/}
-                      {/*      <CommandInput placeholder="Select new role..." />*/}
-                      {/*      <CommandList>*/}
-                      {/*        <CommandEmpty>No roles found.</CommandEmpty>*/}
-                      {/*        <CommandGroup>*/}
-                      {/*          <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
-                      {/*            <p>Viewer</p>*/}
-                      {/*            <p className="text-sm text-muted-foreground">*/}
-                      {/*              Can view and comment.*/}
-                      {/*            </p>*/}
-                      {/*          </CommandItem>*/}
-                      {/*          <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
-                      {/*            <p>Developer</p>*/}
-                      {/*            <p className="text-sm text-muted-foreground">*/}
-                      {/*              Can view, comment and edit.*/}
-                      {/*            </p>*/}
-                      {/*          </CommandItem>*/}
-                      {/*          <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
-                      {/*            <p>Billing</p>*/}
-                      {/*            <p className="text-sm text-muted-foreground">*/}
-                      {/*              Can view, comment and manage billing.*/}
-                      {/*            </p>*/}
-                      {/*          </CommandItem>*/}
-                      {/*          <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
-                      {/*            <p>Owner</p>*/}
-                      {/*            <p className="text-sm text-muted-foreground">*/}
-                      {/*              Admin-level access to all resources.*/}
-                      {/*            </p>*/}
-                      {/*          </CommandItem>*/}
-                      {/*        </CommandGroup>*/}
-                      {/*      </CommandList>*/}
-                      {/*    </Command>*/}
-                      {/*  </PopoverContent>*/}
-                      {/*</Popover>*/}
-                    </CardContent>
-                  </Card>
-                ))}
+                        {/*<ScrollArea*/}
+                        {/*  // type={'always'}*/}
+                        {/*  className={*/}
+                        {/*    'flex-[1_0_200px] rounded-md border text-base ' +*/}
+                        {/*    // fix radix viewport display*/}
+                        {/*    '[&>[data-radix-scroll-area-viewport]>div]:!flex ' +*/}
+                        {/*    '[&>[data-radix-scroll-area-viewport]>div]:flex-col ' +*/}
+                        {/*    '[&>[data-radix-scroll-area-viewport]>div]:gap-3 ' +*/}
+                        {/*    '[&>[data-radix-scroll-area-viewport]>div]:p-4 '*/}
+                        {/*  }*/}
+                        {/*>*/}
+                        {/*  <ul>*/}
+                        {/*    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((el) => (*/}
+                        {/*      <li>{el}</li>*/}
+                        {/*    ))}*/}
+                        {/*  </ul>*/}
+                        {/*</ScrollArea>*/}
+
+                        {/*<Popover>*/}
+                        {/*  <PopoverTrigger asChild={true}>*/}
+                        {/*    <Button variant="outline" className="ml-auto">*/}
+                        {/*      Owner{' '}*/}
+                        {/*      <ChevronDownIcon className="ml-2 h-4 w-4 text-muted-foreground" />*/}
+                        {/*    </Button>*/}
+                        {/*  </PopoverTrigger>*/}
+                        {/*  <PopoverContent className="p-0" align="end">*/}
+                        {/*    <Command>*/}
+                        {/*      <CommandInput placeholder="Select new role..." />*/}
+                        {/*      <CommandList>*/}
+                        {/*        <CommandEmpty>No roles found.</CommandEmpty>*/}
+                        {/*        <CommandGroup>*/}
+                        {/*          <div className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
+                        {/*            <p>Viewer</p>*/}
+                        {/*            <p className="text-sm text-muted-foreground">*/}
+                        {/*              Can view and comment.*/}
+                        {/*            </p>*/}
+                        {/*          </div>*/}
+                        {/*          <div className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
+                        {/*            <p>Developer</p>*/}
+                        {/*            <p className="text-sm text-muted-foreground">*/}
+                        {/*              Can view, comment and edit.*/}
+                        {/*            </p>*/}
+                        {/*          </div>*/}
+                        {/*          <div className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
+                        {/*            <p>Billing</p>*/}
+                        {/*            <p className="text-sm text-muted-foreground">*/}
+                        {/*              Can view, comment and manage billing.*/}
+                        {/*            </p>*/}
+                        {/*          </div>*/}
+                        {/*          <div className="teamaspace-y-1 flex flex-col items-start px-4 py-2">*/}
+                        {/*            <p>Owner</p>*/}
+                        {/*            <p className="text-sm text-muted-foreground">*/}
+                        {/*              Admin-level access to all resources.*/}
+                        {/*            </p>*/}
+                        {/*          </div>*/}
+                        {/*        </CommandGroup>*/}
+                        {/*      </CommandList>*/}
+                        {/*    </Command>*/}
+                        {/*  </PopoverContent>*/}
+                        {/*</Popover>*/}
+                      </CardContent>
+                    </Card>
+                  ))}
+                </ScrollArea>
               </div>
             </AccordionContent>
           </AccordionItem>
