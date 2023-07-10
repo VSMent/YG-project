25c87bf31b3f58d79d28e871edd2792a27b069b7
VSMent <vsmentvs@gmail.com>
2023-07-11 00:56:12 +0300

    update main page overview with Ukrainian months, currency, values

--- src/shadcn-ui/components/examples/overview.tsx
+++ src/shadcn-ui/components/examples/overview.tsx
@@ -4,52 +4,52 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 
 const data = [
   {
-    name: 'Jan',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Січень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Feb',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Лютий',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Mar',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Березень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Apr',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Квітень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'May',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Травень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Jun',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Червень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Jul',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Липень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Aug',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Серпень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Sep',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Вересень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Oct',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Жовтень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Nov',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Листопад',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
   {
-    name: 'Dec',
-    total: Math.floor(Math.random() * 5000) + 1000,
+    name: 'Грудень',
+    total: Math.floor(Math.random() * 23000) + 5000,
   },
 ]
 
@@ -69,7 +69,9 @@ export function Overview() {
           fontSize={12}
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value: number) => `$${value}`}
+          tickFormatter={(value: number) =>
+            `${value.toLocaleString('uk-ua')} ₴`
+          }
         />
         <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
       </BarChart>
