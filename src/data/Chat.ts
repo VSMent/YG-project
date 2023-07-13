1e138d1d6b5bcb59282ce0982625cc5a32ddf580
VSMent <vsmentvs@gmail.com>
2023-07-14 01:26:31 +0300

    breaking! convert Chat class to type

--- src/data/Chat.ts
+++ src/data/Chat.ts
@@ -1,38 +1,16 @@
-class Message {
-  time: string
-  author: string
+type Message = {
+  time: Date
+  authorEmail: string
   body: string
-
-  constructor(_author: string, _body: string, _time: string | null = null) {
-    this.author = _author
-    this.body = _body
-    this.time = _time
-      ? _time
-      : new Date().toISOString().replace('T', ' ').split('.')[0]
-  }
 }
-
-class Chat {
+type Participants = {
+  employee: { email: string; name: string }
+  user: { email: string; name: string }
+}
+type Chat = {
   id: number
-  participants: string[]
+  participants: Participants
   messages: Message[]
-  static lastId = 0
-
-  constructor(
-    participant1: string,
-    participant2: string,
-    _messages: Message[] = []
-  ) {
-    this.id = Chat.lastId + 1
-    this.participants = [participant1, participant2]
-    this.messages = _messages
-    Chat.lastId = this.id
-  }
-
-  addMessage = (author: string, body: string, time: string | null = null) => {
-    this.messages.push(new Message(author, body, time))
-  }
 }
 
-export default Chat
-export { Message }
+export type { Chat, Message, Participants }
