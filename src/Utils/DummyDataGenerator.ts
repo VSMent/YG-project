1d5533fbe9432f158bc2c0883883380902ea1e15
VSMent <vsmentvs@gmail.com>
2023-07-14 03:30:58 +0300

    remove unused stuff

--- src/Utils/DummyDataGenerator.ts
+++ src/Utils/DummyDataGenerator.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
-import { User, Role, Department } from '../data/User'
-import { Chat, Message } from '../data/Chat'
+import { User, Department } from '../data/User'
+import { Chat } from '../data/Chat'
 import chatsData from '../data/chats.json'
 import peopleData from '../data/people.json'
 import hashPassword from './HashPassword'
@@ -10,68 +10,66 @@ const useDummyUserData = () => {
   const { users, addUserObj } = useUserStore()
   const generatedUsers: User[] = []
 
-  console.log('dummy users', generatedUsers)
-
-  useEffect(() => {
+  // useEffect(() => {
+  if (users.length == 0) {
     const add = async () => {
-      if (users.length == 0) {
-        await Promise.all([
-          ...peopleData.predefined.map(async (userData) => {
-            const common = {
-              login: userData.login,
-              pass: await hashPassword(userData.pass),
-              firstname: userData.firstname,
-              lastname: userData.lastname,
-            }
+      await Promise.all([
+        ...peopleData.predefined.map(async (userData) => {
+          const common = {
+            login: userData.login,
+            pass: await hashPassword(userData.pass),
+            firstname: userData.firstname,
+            lastname: userData.lastname,
+          }
 
-            if (userData.role == 'admin' || userData.role == 'user')
-              generatedUsers.push({
-                ...common,
-                role: userData.role,
-              })
-            else if (userData.role == 'employee')
-              generatedUsers.push({
-                ...common,
-                role: userData.role,
-                department: <Department>userData.department,
-              })
-          }),
+          if (userData.role == 'admin' || userData.role == 'user')
+            generatedUsers.push({
+              ...common,
+              role: userData.role,
+            })
+          else if (userData.role == 'employee')
+            generatedUsers.push({
+              ...common,
+              role: userData.role,
+              department: <Department>userData.department,
+            })
+        }),
 
-          ...peopleData.usernames.map(async (username) => {
-            const common = {
-              login: `${username}@${
-                Math.random() > 0.5 ? 'gmail.com' : 'outlook.com'
-              }`,
-              pass: await hashPassword(username),
-              firstname:
-                peopleData.firstnames[
-                  Math.floor(Math.random() * peopleData.firstnames.length)
-                ],
-              lastname:
-                peopleData.lastnames[
-                  Math.floor(Math.random() * peopleData.lastnames.length)
-                ],
-            }
+        ...peopleData.usernames.map(async (username) => {
+          const common = {
+            login: `${username}@${
+              Math.random() > 0.5 ? 'gmail.com' : 'outlook.com'
+            }`,
+            pass: await hashPassword(username),
+            firstname:
+              peopleData.firstnames[
+                Math.floor(Math.random() * peopleData.firstnames.length)
+              ],
+            lastname:
+              peopleData.lastnames[
+                Math.floor(Math.random() * peopleData.lastnames.length)
+              ],
+          }
 
-            if (Math.random() > 0.8)
-              generatedUsers.push({
-                ...common,
-                role: 'employee',
-                department: Math.random() > 0.5 ? 'marketing' : 'personnel',
-              })
-            else
-              generatedUsers.push({
-                ...common,
-                role: 'user',
-              })
-          }),
-        ])
+          if (Math.random() > 0.8)
+            generatedUsers.push({
+              ...common,
+              role: 'employee',
+              department: Math.random() > 0.5 ? 'marketing' : 'personnel',
+            })
+          else
+            generatedUsers.push({
+              ...common,
+              role: 'user',
+            })
+        }),
+      ])
 
-        generatedUsers.forEach((user) => addUserObj(user))
-      }
+      generatedUsers.forEach((user) => addUserObj(user))
     }
     add().catch(console.log)
-  })
+  }
+  // })
 }
 const useDummyChatData = () => {
   const { users } = useUserStore()
@@ -79,41 +77,41 @@ const useDummyChatData = () => {
   const kindOfRealUsers = users.filter((u) => u.role == 'user')
   const kindOfRealEmployees = users.filter((u) => u.role == 'employee')
 
-  const generatedChats: Chat[] = []
-  chatsData.forEach((chatDatum) => {
-    const employee =
-      kindOfRealEmployees[
-        Math.floor(Math.random() * kindOfRealEmployees.length)
-      ]
-    const user =
-      kindOfRealUsers[Math.floor(Math.random() * kindOfRealUsers.length)]
+  if (chats.length == 0) {
+    // const generatedChats: Chat[] = []
+    chatsData.forEach((chatDatum) => {
+      const employee =
+        kindOfRealEmployees[
+          Math.floor(Math.random() * kindOfRealEmployees.length)
+        ]
+      const user =
+        kindOfRealUsers[Math.floor(Math.random() * kindOfRealUsers.length)]
 
-    const c: Chat = {
-      id: 0,
-      participants: {
-        employee: { email: employee.login, name: employee.firstname },
-        user: { email: user.login, name: user.firstname },
-      },
-      messages: [],
-    }
-    chatDatum.forEach((messageDatum) => {
-      c.messages.push({
-        authorEmail: messageDatum[0] == 'e' ? employee.login : user.login,
-        body: messageDatum[1],
-        time: new Date(messageDatum[2]),
-      })
-    })
-    generatedChats.push(c)
-  })
-
-  useEffect(() => {
-    const add = async () => {
-      if (chats.length == 0) {
-        generatedChats.forEach((c) => addChat(c))
+      const c: Chat = {
+        id: 0,
+        participants: {
+          employee: { email: employee.login, name: employee.firstname },
+          user: { email: user.login, name: user.firstname },
+        },
+        messages: [],
       }
-    }
-    add().catch(console.log)
-  })
+      chatDatum.forEach((messageDatum) => {
+        c.messages.push({
+          authorEmail: messageDatum[0] == 'e' ? employee.login : user.login,
+          body: messageDatum[1],
+          time: new Date(messageDatum[2]),
+        })
+      })
+      // generatedChats.push(c)
+      addChat(c)
+    })
+  }
+  // useEffect(() => {
+  //   console.log('chats effect')
+  //   if (chats.length == 0) {
+  //     // generatedChats.forEach((c) => addChat(c))
+  //   }
+  // })
 }
 
 export { useDummyUserData, useDummyChatData }
