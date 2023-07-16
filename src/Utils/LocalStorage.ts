b096a3950fb279cfac1ae65f76f15dfd055970ce
VSMent <vsmentvs@gmail.com>
2023-07-16 19:55:47 +0300

    remove unused LocalStorage.ts

--- src/Utils/LocalStorage.ts
+++
@@ -1,9 +0,0 @@
-const saveToLS = (key: string, data: object) => {
-  localStorage.setItem(key, JSON.stringify(data))
-}
-const loadFromLS = (key: string) => {
-  const dataString = localStorage.getItem(key) ?? null
-  return dataString ? JSON.parse(dataString) : dataString
-}
-
-export { saveToLS, loadFromLS }
