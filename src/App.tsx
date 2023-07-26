6849dfffdfee42dfdd5f81e2ec1598dd1737ae8f
VSMent <vsmentvs@gmail.com>
2023-07-26 20:54:56 +0300

    remove old comment from App

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,6 @@ import {
   useDummyUserData,
 } from '@hooks/DummyDataGenerator'
 
-// <header className="App-header">
-//   <img src={logo} className="App-logo" alt="logo" />
-//   <p>
-//     <code>src/App.js</code> and save to reload.
-//   </p>
-//   <a
-//     className="App-link"
-//     href="https://reactjs.org"
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     Learn React
-//   </a>
-//   <button type="button" onClick={()=>{saveToLS('key',[1,2,{"asd":1}])}}>SAve</button>
-// </header>
-
 function App() {
   useDummyUserData()
   useDummyChatData()
